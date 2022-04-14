bc662056cb5038f3368eeee5a47fbe9a9ce3ea0d
Tommaso Corato <tommaso.corato@glovoapp.com>
2022-04-14 13:26:25 +0200

    added docstrings to selection adn translation

--- genetic_algorithm/reproduction/selection.py
+++ genetic_algorithm/reproduction/selection.py
@@ -1,15 +1,55 @@
 import numpy as np
 import random
+import warnings
 
 
 class Selection:
-    def __init__(self, selection_method, population, reproduction_rate):
+    """
+        Class that performs the selection over the whole population
+        Attributes
+        ----------
+        population: list
+                    the list that contains all the Chromosomes that make up the population
+        selection_method: str
+                          the method that we can use to perform the selection of the parents, it can be "roulette_wheel",
+                          "stochastic_universal_sampling", "tournament", "rank" or "random"
+        reproduction_rate: flaat
+                           the rate at which the population reproduces itself, the number of children is equal to number
+                           of chromosome in the generation * reproduction_rate
+        Methods
+        -------
+        Private:
+        self._roulette_wheel_selection(): it performs the roulette wheel selection on the population to get the parents
+        self._stochastic_universal_sampling(): it performs the stochastic universal sampling selection on the
+                                               population to get the parents
+        self._tournament_selection(tournament_size): it performs the tournament selection on the population
+                                                     to get the parents
+        self._rank_selection(): it performs the rank selection on the population to get the parents
+        self._random_selection(): it performs the random selection on the population to get the parents
+        self._get_subset_sum(gene_num): it adds up the total fitness of the population up to the gene_num specified
+        Public:
+            self.perform_selection(tournament_size): it performs the selection by feeding the parents to the
+                                                    previously specified selection method
+    """
+
+    def __init__(self, selection_method: str, population: list, reproduction_rate: float) -> None:
         self.selection_method = selection_method
         self.population = population
         self.reproduction_rate = reproduction_rate
-        self.n_parents = len(self.population) * self.reproduction_rate
+        self.n_parents: int = int(len(self.population) * self.reproduction_rate)
 
-    def perform_selection(self, tournament_size=None):
+    def perform_selection(self, tournament_size: int = None) -> np.array:
+        """
+        it performs the selection by feeding the parents to the previously specified selection method
+        Parameters
+        ----------
+        tournament_size: int
+                         the size of the tournament that we use for the "tournament" selection
+        Returns
+        -------
+        np.array
+        an array that contains the selected parents for the reproduction
+        """
         if self.selection_method == "roulette_wheel":
             parents = self._roulette_wheel_selection()
         elif self.selection_method == "stochastic_universal_sampling":
@@ -28,7 +68,14 @@ class Selection:
 
         return parents
 
-    def _roulette_wheel_selection(self):
+    def _roulette_wheel_selection(self) -> np.array:
+        """
+        it performs the roulette wheel selection on the population to get the parents
+        Returns
+        -------
+        np.array
+        an array that contains the selected parents for the reproduction
+        """
         all_parents = []
         while len(all_parents) < self.n_parents:
 
@@ -40,7 +87,14 @@ class Selection:
 
         return np.array(all_parents)
 
-    def _stochastic_universal_sampling(self):
+    def _stochastic_universal_sampling(self) -> np.array:
+        """
+        it performs the stochastic universal sampling selection on the population to get the parents
+        Returns
+        -------
+        np.array
+        an array that contains the selected parents for the reproduction
+        """
         total_fitness = sum([chromosome.fitness for chromosome in self.population])
         point_distance = total_fitness / self.n_parents
         start_point = random.uniform(0, point_distance)
@@ -59,7 +113,18 @@ class Selection:
 
         return np.array(parents)
 
-    def _get_subset_sum(self, gene_num):
+    def _get_subset_sum(self, gene_num: int) -> float:
+        """
+        it adds up the total fitness of the population up to the gene_num specified
+        Parameters
+        ----------
+        gene_num: the position up to which to sum the fitness for the population
+
+        Returns
+        -------
+        float
+        the total fitness for the population up to gene num
+        """
         subset_sum = 0.0
         i = 0
 
@@ -69,6 +134,18 @@ class Selection:
         return subset_sum
 
     def _tournament_selection(self, tournament_size=4):
+        """
+        it performs the tournament selection on the population to get the parents
+        Parameters
+        ----------
+        tournament_size: int
+                         the size of the tournament that we use for the tournament selection
+
+        Returns
+        -------
+        np.array
+        an array that contains the selected parents for the reproduction
+        """
         all_parents = []
         while len(all_parents) < self.n_parents:
             parents = random.choices(self.population, k=tournament_size)
@@ -78,6 +155,13 @@ class Selection:
         return np.array(all_parents)
 
     def _rank_selection(self):
+        """
+        it performs the rank selection on the population to get the parents
+        Returns
+        -------
+        np.array
+        an array that contains the selected parents for the reproduction
+        """
         parents = sorted(self.population, key=lambda gene: gene.fitness, reverse=True)
         best_parents = parents[:self.n_parents]
 
@@ -86,6 +170,14 @@ class Selection:
         return np.array(best_parents)
 
     def _random_selection(self):
+        """
+        it performs the random selection on the population to get the parents
+        Returns
+        -------
+        np.array
+        an array that contains the selected parents for the reproduction
+        """
+        warnings.warn("The random selections leads to a stochastic search for the parameters")
         parents = random.choices(self.population, k=self.n_parents)
 
         return np.array(parents)
